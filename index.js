a494d9209306b1e644b6035c8c04e3bab767b17d
Nick Bogdanov <frague@mail.ru>
2016-11-01 22:22:21 +0300

    Fixes rendering errors

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function errorResponse(response, code, error) {
 
 var service = server.listen(system.env.PORT || 8088, function (request, response) {
     var params = parseGET(request.url),
-        url = params.url,
+        url = decodeURIComponent(params.url),
         selector = params.selector || 'body',
         page = require('webpage').create();
 
@@ -40,13 +40,58 @@ var service = server.listen(system.env.PORT || 8088, function (request, response
         return formResponse(response);
     }
 
-    page.onConsoleMessage = function(msg) {
-        console.log('CONSOLE: ' + msg);
-    };
-
     page.viewportSize = { width: 1024, height: 600 };
 
-    console.log('Requested URL:', decodeURIComponent(url), ' with selector:', selector);
+    // page.onResourceRequested = function (request) {
+    //     system.stderr.writeLine('= onResourceRequested()');
+    //     system.stderr.writeLine('  request: ' + JSON.stringify(request, undefined, 4));
+    // };
+
+    // page.onResourceReceived = function(response) {
+    //     system.stderr.writeLine('= onResourceReceived()' );
+    //     system.stderr.writeLine('  id: ' + response.id + ', stage: "' + response.stage + '", response: ' + JSON.stringify(response));
+    // };
+
+    // page.onLoadStarted = function() {
+    //     system.stderr.writeLine('= onLoadStarted()');
+    //     var currentUrl = page.evaluate(function() {
+    //         return window.location.href;
+    //     });
+    //     system.stderr.writeLine('  leaving url: ' + currentUrl);
+    // };
+
+    // page.onLoadFinished = function(status) {
+    //     system.stderr.writeLine('= onLoadFinished()');
+    //     system.stderr.writeLine('  status: ' + status);
+    // };
+
+    // page.onNavigationRequested = function(url, type, willNavigate, main) {
+    //     system.stderr.writeLine('= onNavigationRequested');
+    //     system.stderr.writeLine('  destination_url: ' + url);
+    //     system.stderr.writeLine('  type (cause): ' + type);
+    //     system.stderr.writeLine('  will navigate: ' + willNavigate);
+    //     system.stderr.writeLine('  from page\'s main frame: ' + main);
+    // };
+
+    // page.onResourceError = function(resourceError) {
+    //     system.stderr.writeLine('= onResourceError()');
+    //     system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
+    //     system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString );
+    // };
+
+    // page.onError = function(msg, trace) {
+    //     system.stderr.writeLine('= onError()');
+    //     var msgStack = ['  ERROR: ' + msg];
+    //     if (trace) {
+    //         msgStack.push('  TRACE:');
+    //         trace.forEach(function(t) {
+    //             msgStack.push('    -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+    //         });
+    //     }
+    //     system.stderr.writeLine(msgStack.join('\n'));
+    // };    
+
+    console.log('Requested URL: ' + url + ' with selector: ' + selector);
 
     page.open(url, function (status) {
         if (status !== 'success') {
